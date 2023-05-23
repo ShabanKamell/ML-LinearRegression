35ea2cfb137774843ea28cee54bea039e5d0bfd1
Shaban <sh3ban.kamel@gmail.com>
2023-05-23 17:30:34 +0300

    Add problems descriptions

--- gradient_descent/gradient_descent_3_var_function.py
+++ gradient_descent/gradient_descent_3_var_function.py
@@ -1,6 +1,19 @@
 import numpy as np
 from gradient_descent_vec import gradient_descent
 
+"""
+Consider the function: f(x, y, z) = sin(x) + cos(y) + sin(z)
+● Explore several initial starting points, and report a list of minimum values for
+this function using gradient descent
+● Import; don’t copy!
+○ from gradient_descent_vec import gradient_descent
+● As an example, one of the outputs should:
+Initially start at [1. 2. 3.5]
+ends at point: [0.22457445 2.67782963 4.21311734]
+with a minimum value of -1.5496155799445872
+● Recall derivative of sin(x) = cos(x), and derivative of cos(x) = -sin(x)
+○ Or use an online calculator
+"""
 
 def f_deriv(x):
     return np.array([
--- gradient_descent/gradient_descent_vec.py
+++ gradient_descent/gradient_descent_vec.py
@@ -1,12 +1,22 @@
 import numpy as np
 
-
+"""
+We implemented the gradient descent for 2 variables (x, y)
+● Do simple changes to let the code process the entire vector of variables,
+rather than just two variables
+○ Test it using the two functions provided
+○ Compare the results with those of the old program
+● Create file: gradient_descent_vec.py
+● def gradient_descent(fderiv, inital_start, step_size = 0.001, precision = 0.00001, max_iter = 1000):
+○ inital_start: numpy array of D values
+○ fderiv: function can take a vector of D values and returns D gradients
+"""
 def gradient_descent(
         fderiv,
         initial_start,
         step_size=0.001,
         precision=0.00003,
-        max_iter=1000):
+        max_iter=10000):
     current_pos = initial_start
     for i in range(max_iter):
         prev_pos = current_pos
